ac450efbc667516f0e4513254cbad37a48ed3d7c
Kristin Cox <kristincox09@yahoo.com>
2013-04-21 20:42:17 -0500

    Finished parsing, started smoothing

--- Huffman/src/com/algorithms/huffman/Huffman.java
+++ Huffman/src/com/algorithms/huffman/Huffman.java
@@ -1,162 +1,138 @@
 package com.algorithms.huffman;
 
-
 import java.util.*;
 import java.io.*;
 
 public class Huffman {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		Scanner in = null; // for input from text file
-//		PrintStream out = null; // for output to html file
-		// dictionary used to store keywords
+	public static void main (String args[]) {
+		
+		Scanner stdin = null;
+		// map to hold words and frequencies
 		BSTDictionary<KeyWord> dictionary = new BSTDictionary<KeyWord>();
-
-		// Make sure the input file exists and can be read
-		try {
-			File inFile = new File("data/washington/first_inaugural_address.txt");
+		
+		File folder = new File("\\Users\\Kristin\\Documents\\GitHub\\CS577-Huffman\\Huffman\\oldest");
+		File[] listOfFiles = folder.listFiles();
+		
+		// process all files in speech directory
+		for (File inFile : listOfFiles) {
+			// check if file exits and can be read
 			if (!inFile.exists() || !inFile.canRead()) {
-				System.err.println("Error: cannot access file " + inFile.getAbsolutePath());
-				System.exit(1);
+				System.out.println("Improper file: " + inFile.getName());
+				System.exit(-1);
 			}
-
-			in = new Scanner(inFile);
-		} catch (FileNotFoundException e) {
-			System.err.println("Error: cannot access file");
-			System.exit(1);
+			try {
+				// open file and parse all words
+				stdin = new Scanner(inFile);
+				parseFile(dictionary, stdin);
+			} catch (FileNotFoundException ex) {
+				System.out.println("Unable to find file: " + inFile.getName());
+				System.exit(-1);
+			}
 		}
-
-//		// Make sure we can write to the given output filename
-//		try {
-//			// write to given file name
-//			File outFile = new File(args[1]);
-//			// warn if it exists
-//			if (outFile.exists()) {
-//				System.err.println("Warning: file " + args[1]
-//						+ " already exists, will be overwritten");
-//			}
-//			// stop if we can't write to the file
-//			if (outFile.exists() && !outFile.canWrite()) {
-//				System.err.println("Error: cannot write to file " + args[1]);
-//				System.exit(1);
-//			}
-//
-//			// open the file to write to
-//			out = new PrintStream(outFile);
-//
-//		} catch (FileNotFoundException e) {
-//			// catch if we can't write
-//			System.err.println("Error: cannot write to file " + args[1]);
-//			System.exit(1);
-//		}
-
-		// Process the input file line by line
-		// Note: the code below just prints out the words contained in each
-		// line. You will need to replace that code with code to generate
-		// the dictionary of KeyWords.
-		while (in.hasNext()) {
-			String line = in.nextLine();
-			List<String> words = parseLine(line);
-
-			// Iterate through all the word on this line
-			KeyWord keyword;
-			for (String word : words) {
-				try {
-					// Try inserting the keyword
-					keyword = new KeyWord(word.toLowerCase());
-					keyword.increment();
-					dictionary.insert(keyword);
-				} catch (DuplicateKeyException e) {
-					// If the keyword is already in the dictionary just
-					// increment the occurrences
-					keyword = dictionary
-							.lookup(new KeyWord(word.toLowerCase()));
-					keyword.increment();
-				}
-			}
-
-		} // end while
 		
 		System.out.println(dictionary.size());
-
-
-	        File inFile = new File("test.txt");
-        	calcFrequency(inFile);
 	}
 	
-	private static List<String> parseLine(String line) {
-		String[] tokens = line.split("[ ]+");
-		ArrayList<String> words = new ArrayList<String>();
-		for (int i = 0; i < tokens.length; i++) { // for each word
-
-			// find index of first digit/letter
-			boolean done = false;
-			int first = 0;
-			String word = tokens[i];
-			while (first < word.length() && !done) {
-				if (Character.isDigit(word.charAt(first))
-						|| Character.isLetter(word.charAt(first)))
-					done = true;
-				else
-					first++;
-			}
-
-			// find index of last digit/letter
-			int last = word.length() - 1;
-			done = false;
-			while (last > first && !done) {
-				if (Character.isDigit(word.charAt(last))
-						|| Character.isLetter(word.charAt(last)))
-					done = true;
-				else
-					last--;
-			}
-
-			// trim from beginning and end of string so that is starts and
-			// ends with a letter or digit
-			word = word.substring(first, last + 1);
-
-			// make sure there is at least one letter in the word
-			done = false;
-			first = 0;
-			while (first < word.length() && !done)
-				if (Character.isLetter(word.charAt(first)))
-					done = true;
-				else
-					first++;
-			if (done)
-				words.add(word);
+	/*
+	 * Add one to the count for every word in all the speeches
+	 */
+	public static void smooth(BSTDictionary<KeyWord> dictionary, Scanner in) {
+		while (in.hasNext()) {
+			String line = in.next();
+			String[] tokens = line.split("[ ]+");
 		}
-		return words;
 	}
 	
-	public static HashMap<String, Double> calcFrequency(File inFile) {
-		Scanner stdin = null;
-		HashMap<String, Double> map = new HashMap<String, Double>();
-		double count = 0;
+	/*
+	 * Takes each line in a file and adds each word to the dictionary, or, if already 
+	 * present, increments its value.
+	 */
+	public static void parseFile(BSTDictionary<KeyWord> dictionary, Scanner in) {
 		
-		try {
-			stdin = new Scanner(inFile);
-		} catch (FileNotFoundException e) {
-			System.out.println("File " + inFile + " not found.");
-			System.exit(0);
+		while (in.hasNext()) {
+			String line = in.next();
+			String[] tokens = line.split("[ ]+");
+			for (String word : tokens) {
+				
+				int first = findFirst(word);
+				int last = findLast(word, first);
+				
+				// trim string so it starts and ends with letter/digit
+				word = word.substring(first, last + 1);
+				
+				boolean done = isLetter(word);
+				
+				// if letter was found, increment its value in the dictionary
+				if (done) {
+					KeyWord keyword;
+					try {
+						// try inserting new keyword
+						keyword = new KeyWord(word.toLowerCase());
+						keyword.increment();
+						dictionary.insert(keyword);
+					} catch (DuplicateKeyException e) {
+						// if already in dictionary, just increment occurences
+						keyword = dictionary.lookup(new KeyWord(word.toLowerCase()));
+						keyword.increment();
+					}
+				}
+			} // end outer while
 		}
-		
-		while (stdin.hasNext()) {
-			String line = stdin.next();
-			String[] tokens = line.split(" ");
-			System.out.println(tokens[0]);
-			for (String word:tokens) {
-				count = map.get(word);
-				count++;
-				map.put(word, count);
-			}
-			System.out.println(tokens[0]);
-		}
-		return new HashMap<String, Double>();
 	}
-
+	
+	/*
+	 * Finds position of first letter or digit in word
+	 */
+	public static int findFirst(String word) {
+		// find index of first digit/letter
+		boolean done = false;
+		int first = 0;
+		while (first < word.length() && !done) {
+			if (Character.isDigit(word.charAt(first)) || Character.isLetter(word.charAt(first))) {
+				done = true;
+			}
+			else {
+				first++;
+			}
+		} // end while
+		
+		return first;
+	}
+	
+	/*
+	 * Finds position of last letter or digit in word
+	 */
+	public static int findLast(String word, int first) {
+		// find index of last digit/letter
+		boolean done = false;
+		int last = word.length() - 1;
+		while (last > first && !done) {
+			if (Character.isDigit(word.charAt(last)) || Character.isLetter(word.charAt(last))) {
+				done = true;
+			}
+			else {
+				last--;
+			}
+		} // end while
+		return last;
+	}
+	
+	/*
+	 * Returns true if there is at least one letter in the given word
+	 */
+	public static boolean isLetter(String word) {
+		// make sure there is at least one letter in the word
+		boolean done = false;
+		int first = 0;
+		while (first < word.length() && !done) {
+			if (Character.isLetter(word.charAt(first))) {
+				done = true;
+			}
+			else {
+				first++;
+			}
+		} // end while
+		return done;
+	}
 }
